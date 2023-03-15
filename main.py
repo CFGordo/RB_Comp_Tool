4a3a607b2d042cd99c3de84a6f9dbcc153f8deaf
CFGordo <111022581+CFGordo@users.noreply.github.com>
2023-03-15 10:39:43 -0400

    team?

--- main.py
+++ main.py
@@ -74,8 +74,9 @@ else:
 
 playerSZN_comp_df = player_comp_df.loc[player_comp_df['Player ID'] == player2]
 playerSZN_comp_df = playerSZN_comp_df.loc[playerSZN_comp_df['Season'] == player3]
-player_team = playerSZN_comp_df['Team'].unique()
-player_tm = str(player_team)
+player_team = playerSZN_comp_df['Team']
+player_team = player_team.drop_duplicates()
+player_tm = player_team.to_string()
 
 riker = {"Criteria": ["Height", "Weight", "Rushing Efficiency", "Rushing Explosiveness", "Receiving Efficiency",
                       "Rec Explosiveness", "Receiving Best", "Team Talent", "Team SP Rating", "NFL Draft Position"],
@@ -228,8 +229,7 @@ nfl_comp = rb_comp_df.loc[rb_comp_df['Games Played'] >= games]
 
 team_only = st.checkbox('Same Team Only')
 if team_only:
-    nfl_comp = nfl_comp.loc[nfl_comp['Team'] == player_team.astype(str)]
-    st.text(nfl_comp.shape)
+    nfl_comp = nfl_comp.loc[nfl_comp['Team'] == player_tm]
 else:
     pass
 
@@ -249,7 +249,7 @@ else:
                            'Receiving Efficiency', 'Rushing Explosiveness', 'Receiving Explosiveness', 'Team Talent',
                            'SP Rating', 'Draft Year', 'NFL Draft Pick', 'NFL PPR PPG', 'Player ID']]
 
-st.text(player_team)
+st.text(player_tm)
 
 row_number = st.number_input('Number of Comps', min_value=0, value=11)
 data2 = draw_grid(
